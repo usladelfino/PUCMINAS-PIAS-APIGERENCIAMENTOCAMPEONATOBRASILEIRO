4de8740a35550744aad693d436efaa7a14f6beaf
usladelfino <40376796+usladelfino@users.noreply.github.com>
2022-06-30 15:57:22 -0300

    Add files via upload

---
+++ MessageSubscriber/MessageSubscriber/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace MessageSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("events", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, eventArgs) =>
+                    {
+                        var body = eventArgs.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(message);
+                    };
+
+                    channel.BasicConsume(queue: "events", autoAck: true, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
